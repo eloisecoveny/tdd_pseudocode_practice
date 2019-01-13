e332ecd3d2425a2a310be128632a7bb56a3682a5
Eloise Coveny <eloisecoveny@gmail.com>
2019-01-13 22:23:03 +0000

    TDD: Pseudocode practice - first pass

---
+++ README.md
@@ -0,0 +1,15 @@
+# TDD / Pseudocode practice; week one.
+
+The object of this exercise is to practice "algorithmic thinking."
+
+You have three tasks, with some very minimal tests set up. Write some more tests if you would like to refine your code further.
+
+Treat this session as a [mob programming](http://underthehood.meltwater.com/blog/2016/06/01/mob-programming/) session.
+
+Before you write any actual code, make sure the students think through the strategy they are going to use. (Perhaps write five or six comments in the method body as their strategy.)
+
+Questions 1 and 2 are intended to let you practice various string, array, integer and float methods.
+
+Question 2 will _probably_ use a while loop, but maybe you could research recursion!
+
+Question 3 should let you practice navigating through hashes and arrays.
---
+++ my_functions.rb
@@ -0,0 +1,55 @@
+require('pry')
+
+def pigify(sentence)
+	# Convert sentence to lower case
+	# split sentence into array of words
+	words_array = sentence.downcase.split()
+
+	# for each word...
+	amended_words = []
+	for word in words_array
+		# Move first letter to the end
+		# Add "ay" to the end of each word
+		amended_words << (word[1..-1] + word[0] + "ay")
+	end
+
+	# join words to make a new string
+	# Capitalize first letter of first word
+	# return sentence
+	return amended_words.join(" ").capitalize()
+
+end
+
+def reduce_number(number)
+	if number > 9
+		# Convert the number into a string and separate the digits
+		split_digits = number.to_s.split(//)
+		sum = 0
+		# For each digit in the number
+		for digit in split_digits
+			# Convert it into an integer and add to to the total "sum"
+			sum += digit.to_i
+		end
+		reduce_number(sum)
+		# # Return the total sum
+		# return sum
+	else
+		return number
+	end
+end
+
+def order_total(order)
+	# Calculate the number of unique books
+	unique_books = order[:items].count()
+	# Calculate the discount
+	discount = 0.05 * (unique_books - 1)
+	# Calculate the total quantity of books
+	total = 0
+	for book in order[:items]
+		total += (book[:quantity] * book[:price])
+	end
+	# Calculate the total order cost including discount
+	total = (total * (1 - discount)).round(2)
+	# Return a string that declares the customer's name and their total including discount
+	return "#{order[:name]}'s total is £#{total}"
+end
